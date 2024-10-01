87d6ecaa4723955087de666a3915fe496cc88fd1
Francisco-delgad0 <143971602+Francisco-delgad0@users.noreply.github.com>
2024-10-01 20:36:48 -0300

    Update PeliculaConHover.js

--- src/componenetes/PeliculaConHover.js
+++ src/componenetes/PeliculaConHover.js
@@ -4,12 +4,12 @@ import { NavLink } from 'react-router-dom';
 
 const PeliculaConHover = ({ pelicula, imagePath }) => {
     return (
-        <div className="col-12 col-sm-6 col-md-4 mb-4"> 
+        <div className="col-12 col-sm-6 col-md-4 mb-4 movie-card"> 
             <div className="overlay">
                 <NavLink to={`/DetallesPelicula/${pelicula.id}`} className="nav-link">
                     <img 
                         src={`${imagePath}/${pelicula.poster_path}`} 
-                        className="card-img-top movie-poster img-fluid" 
+                        className="card-img-top movie-poster img-fluid movie-card-image"  
                         alt={pelicula.title} 
                     /> 
                 </NavLink>
