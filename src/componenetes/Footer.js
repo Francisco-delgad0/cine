81b291158e183cf02d1d4fc6fa0d7f5b5cc12491
ZetaBeeM <thisnameisnotpermitted@gmail.com>
2024-10-02 02:20:04 -0300

    Sección "Sobre nosotros" agregado al footer

--- src/componenetes/Footer.js
+++ src/componenetes/Footer.js
@@ -5,12 +5,13 @@ import "../App.css"
 
 const Footer = () => {
   return (
-    <footer className="custom-footer pt-5 pt-4 ">
+    <footer className="custom-footer">
       <div className="container text-center text-md-start">
         <div class="row text center text-md-start">
           <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
             <p>@ 2024 Parias. Todos los derechos reservados.</p>
           </div>      
+         <p>Sobre nosotros: Operado por un equipo de tres cinéfilos dedicados, nuestro objetivo es ofrecer una selección diversa de películas, desde grandes éxitos de taquilla hasta gemas independientes y producciones locales, en un ambiente acogedor y accesible.</p>
          </div>
       </div>
     </footer>
