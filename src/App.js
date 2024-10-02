3d37aaee27e69275971d39e5d105064e2b9b7ffc
zKodzz <belmar@sent.com>
2024-10-01 23:00:04 -0400

    Cine.js, Cartelera.js y Confiteria.js agregadas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from "./componenetes/Footer"; 
 import Navbar from "./componenetes/Navbar";
 import Homepage from "./paginas/Homepage";
+import Cartelera from "./paginas/cartelera"
+import Cines from "./paginas/cines";
+import Confiteria from "./paginas/confiteria";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/Homepage" element={<Homepage />} />
+          <Route path="/Cartelera" element={<Cartelera />} />
+          <Route path="/Cines" element={<Cines />} />
+          <Route path="/Confiteria" element={<Confiteria />} />
         </Routes>
         <Footer/>
       </div>
--- src/componenetes/Navbar.js
+++ src/componenetes/Navbar.js
@@ -10,7 +10,7 @@ function Navbar(){
       <div class="container-fluid">        
         <NavLink to="/Homepage" className="navbar-brand text-white" activeclassname="is-active" exact="true">CineApp</NavLink>
         <NavLink to="/Cartelera" className="nav-link text-white" activeClassName="is-active">Cartelera</NavLink>
-        <NavLink to="/Localidades" className="nav-link text-danger" activeClassName="isactive">Cines</NavLink>
+        <NavLink to="/Cines" className="nav-link text-danger" activeClassName="isactive">Cines</NavLink>
         <NavLink to="/Confiteria" className="nav-item" activeClassName="is-active">Confitería</NavLink>
         <NavLink to="/Login" className="nav-button">Iniciar Sesión</NavLink>
         <span className="nav-indicador"></span>
